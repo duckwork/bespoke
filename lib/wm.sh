fe9453411d2472a4de223a7faf9caba05b90aa21
Case Duckworth <cased123@gmail.com>
2017-04-07 16:57:26 -0500

    Separate out `wm` functions to lib/wm.sh
    Also: edit works with a temporary file so the wm will stay working until
    you exit the editor.  Also I got rid of a lame option: wm -i

---
+++ lib/wm.sh
@@ -0,0 +1,117 @@
+#! /usr/bin/env dash
+# WM utility functions
+
+# Utility functions
+runf()
+{ # runf <cmd> <args>
+  f="$1"; shift;
+  case "$1" in
+    0x*) wid="$1" ;;
+    *)
+      [ $# -ge 2 ] || die $E_ARG "Not enough arguments"
+      cmd="$BIN/$1"; shift;
+      if [ -x "$cmd" ]; then
+        wid="$($cmd $@)";
+      else
+        die $E_EXE "Unknown command: \"$cmd\"";
+      fi
+      ;;
+  esac
+  logx $f $wid;
+}
+
+edit()
+{ # Edit a file in wm
+  f="$BIN/$1";
+  t="$(mktemp $TMP/${f##*/}-edit.XXXXXX)";
+  cp $f $t;
+  ${EDITOR:-vi} $t;
+  if silent diff -q $t $f; then
+    # file was not changed
+    rm $t;
+  else
+    # file was changed
+    mv $t $f;
+  fi
+}
+
+# Atomic actions
+focus() 
+{ # focus a window
+  # . $LIB/config.sh
+  focus_wid() {
+    # oldw=$(pfw);
+    if wattr "$1"; then
+      wtf "$1";
+      chwso -r "$1";
+      # set_borders "$w" "$1";
+    fi
+  }
+  runf focus_wid "$@";
+}
+
+map()
+{ # map a window
+  map_wid() {
+    if wattr "$1" && ! wattr m "$1"; then
+      mapw -m "$1";
+      focus "$1";
+    fi
+  }
+  runf map_wid "$@";
+}
+
+hide()
+{ # unmap a window
+  hide_wid() {
+    if wattr "$1" && wattr m "$1"; then
+      mapw -u "$1";
+      # TODO deal with focus
+    fi
+  }
+  runf hide_wid "$@";
+}
+
+ignore()
+{ # set a window's override_redirect property
+  ignore_wid() {
+    if wattr "$1" && ! wattr o "$1"; then
+      ignw -s "$1";
+      # TODO deal with focus
+    fi
+  }
+  runf ignore_wid "$@";
+}
+
+notice()
+{ # unset a window's override_redirect property
+  notice_wid() {
+    if wattr "$1" && wattr o "$1"; then
+      ignw -r "$1";
+      focus "$1";
+    fi
+  }
+  runf notice_wid "$@";
+}
+
+raise()
+{ # raise a window to the top of the stack
+  raise_wid() {
+    if wattr "$1"; then
+      chwso -r "$1";
+      focus "$1";
+    fi
+  }
+  runf raise_wid "$@";
+}
+
+lower()
+{ # lower a window to the bottom of the stack
+  lower_wid() {
+    if wattr "$1"; then
+      chwso -l "$1";
+      # TODO deal with focus
+    fi
+  }
+  runf lower_wid "$@";
+}
