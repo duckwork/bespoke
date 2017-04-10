caf42ae7c5d1a5bd2ee94006c33d9ab5f027560e
Case Duckworth <cased123@gmail.com>
2017-04-09 20:20:57 -0500

    Comment out config.sh (it's bpptptp)

--- lib/config.sh
+++ lib/config.sh
@@ -3,24 +3,24 @@
 # set config stuff
 
 # TODO edit to work with schemes (see config folder)
-set_borders()
-{ # set borders according to config
-  . $WMD/config
-  case $1 in
-    0x*)
-      [ $# -eq 2 ] || return 1;
-      chwb -s $border_width -c $border_color_normal $1;
-      chwb -s $border_width -c $border_color_focus $2;
-      ;;
-    all)
-      for w in $TMP/clients/mapped; do
-        chwb -s $border_width $w;
-        if [ "$w" = "$(pfw)" ]; then
-          chwb -c $border_color_focus $w;
-        else
-          chwb -c $border_color_normal $w;
-        fi
-      done
-    ;;
-esac
-}
+# set_borders()
+# { # set borders according to config
+#   . $WMD/config
+#   case $1 in
+#     0x*)
+#       [ $# -eq 2 ] || return 1;
+#       chwb -s $border_width -c $border_color_normal $1;
+#       chwb -s $border_width -c $border_color_focus $2;
+#       ;;
+#     all)
+#       for w in $TMP/clients/mapped; do
+#         chwb -s $border_width $w;
+#         if [ "$w" = "$(pfw)" ]; then
+#           chwb -c $border_color_focus $w;
+#         else
+#           chwb -c $border_color_normal $w;
+#         fi
+#       done
+#     ;;
+# esac
+# }
